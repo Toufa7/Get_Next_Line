09441fd9c43550a5f3b36b5aebbded59442804cf
PØM$ <90482193+Toufa7@users.noreply.github.com>
2022-02-05 09:57:13 +0100

    Done 100%

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: otoufah <otoufah@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 17:45:50 by otoufah           #+#    #+#             */
+/*   Updated: 2022/02/03 09:39:02 by otoufah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+size_t	ft_strlen(char *str);
+size_t	ft_find_len(char *str, char what);
+char	*ft_strchar(char *str, char what);
+char	*ft_strjoin(char *str1, char *str2);
+char	*ft_substr(char *str, size_t start, size_t how);
+char	*cutting_until_newline(char *str);
+char	*keeping_what_after_newline(char *str);
+char	*read_and_join(int fd, char *save);
+char	*get_next_line(int fd);
+
+#endif
